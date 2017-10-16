e4818205d214d9f0b477d41313db4027300b50d3
Murilo Fujita <murilofujita@fujita.com>
2017-10-16 17:13:38 -0200

    Retomada do Pacman em 16/10/2017.

---
+++ PacMan/Assets/Scripts/Demolidor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demolidor : MonoBehaviour 
+{
+	void Start () 
+    {
+		
+	}
+	
+	void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector2.up);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector2.down);
+        }
+	}
+}
