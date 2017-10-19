84f964effaf08fd2048a0ef5c28225cc18f2fb69
Murilo Fujita <murilofujita@fujita.com>
2017-10-19 16:08:21 -0200

    Movimento do Pacman corrigido e um fantasma com 20 waypoints.

--- PacMan/Assets/Scripts/Ghost.cs
+++ PacMan/Assets/Scripts/Ghost.cs
@@ -4,27 +4,33 @@ using UnityEngine;
 
 public class Ghost : MonoBehaviour 
 {
-    public Transform destino;
+    public Transform[] destino;
+    int i=0;
     public float speed = 0.3f;
 
     void FixedUpdate () 
     {
-        if (transform.position != destino.position)
+        if (transform.position != destino[i].position)
         {
-            Vector2 p = Vector2.MoveTowards(transform.position, destino.position, speed);
-            GetComponent<Rigidbody2D>().MovePosition(p);
+            Vector2 p = Vector2.MoveTowards(transform.position, destino[i].position, speed);
+            GetComponent<Rigidbody2D>().MovePosition(p);                
         }
+        else
+        {
+            i = (i + 1) % destino.Length;
+        }       
 
-        Vector2 dir = destino.position - transform.position;
+        Vector2 dir = destino[i].position - transform.position;
         GetComponent<Animator>().SetFloat("dirX", dir.x);
         GetComponent<Animator>().SetFloat("dirY", dir.y);
 	}
 
-    void OnTriggerEnter2d(Collider2D co)
+    void OnCollisionEnter2D(Collision2D co)
     {
-        if (co.name == "pacman")
+        if (co.gameObject.name == "pacman")
         {
             Destroy(co.gameObject);
+            Destroy(GetComponent<Ghost>());
         }
     }
 }
---
+++ PacMan/Assets/Scripts/PacmanMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacmanMove : MonoBehaviour 
+{
+    public float speed = 0.01f;
+    Vector2 dest = Vector2.zero;
+    public Animator animator;
+    	
+	void Start () 
+    {        
+        animator = GetComponent<Animator>();
+	}	
+	
+	void FixedUpdate () 
+    {
+        float posX = Input.GetAxisRaw("Horizontal");
+        float posY = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * speed);
+            GetComponent<Animator>().SetFloat("dirY", posY);
+            GetComponent<Animator>().SetFloat("dirX", 0);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * speed);
+            GetComponent<Animator>().SetFloat("dirY", posY);
+            GetComponent<Animator>().SetFloat("dirX", 0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed);
+            GetComponent<Animator>().SetFloat("dirX", posX);
+            GetComponent<Animator>().SetFloat("dirY", 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed);
+            GetComponent<Animator>().SetFloat("dirX", posX);
+            GetComponent<Animator>().SetFloat("dirY", 0);
+        }
+	}
+}
